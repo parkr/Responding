380113815082c27b97a54a82290657c47261b02a
Parker Moore <parkrmoore@gmail.com>
2011-06-29 01:24:13 -0400

    added ability to create and keep track of messages to different people, add people and check for preexisting message.txt and response.txt from v1.0

--- PMMessage.h
+++ PMMessage.h
@@ -17,21 +17,52 @@
 #define PMCopiedText @"Copied to clipboard."
 #define PMCopiedFailedText @"Copy to clipboard failed."
 
+#define PMPeopleFoundText @"Respondees have been found."
+#define PMNoPeopleFoundTitle @"No Respondees"
+#define PMNoPeopleFoundText @"No existing repondees were found."
+#define PMResponseFoundButNoPeopleText @"A message and response pair was found, but no name. Who is it for?"
+#define PMNewPersonText @"To whom are you writing this response?"
+#define PMSelectAPersonText @". Please select a recipient or create a new one to begin."
+#define PMNewPersonCreated @"New recipient created."
+
 #define PMConcatenateToString(old, add) ([old stringByAppendingString:add])
 #define PMErrorSavingFiles(errorMessage) (@"Error occurred during saving: %@", errorMessage)
+
+#define PMCurrentPerson [[peopleArray objectAtIndex:indexOfCurrentPerson] lowercaseString]
+#define PMMessageTo(name) [[NSString alloc] initWithFormat:@"message_to_%@.txt", name]
+#define PMResponseTo(name) [[NSString alloc] initWithFormat:@"response_to_%@.txt", name]
+
+#define PMHelveticaFont [NSFont fontWithName:@"Helvetica" size:16.0]
+#define PMKozukaFont [NSFont fontWithName:@"Kozuka Mincho Pro" size:16.0]
 
 @interface PMMessage : NSObject {
 	IBOutlet NSTextField *message;
 	IBOutlet NSTextField *response;
 	IBOutlet NSTextField *status;
-	NSString *messageFile;
-	NSString *responseFile;
+	IBOutlet NSPopUpButton *people;
+	IBOutlet NSButton *save;
+	IBOutlet NSButton *copy;
 	NSString *messageText;
 	NSString *responseText;
+	NSString *messageFile;
+	NSString *responseFile;
+	NSString *peopleFile;
+	NSMutableArray *peopleArray;
+	int indexOfCurrentPerson;
 }
 
+- (void)enableInterface;
+- (void)disableInterface;
+
 - (IBAction)save:(id)sender;
+- (BOOL)savePeopleToFile;
 - (IBAction)copyResponseToClipboard:(id)sender;
-- (void)loadFromFiles;
+
+- (IBAction)chooseFont:(id)sender;
+- (void)setFont:(NSFont *)aFont;
+
+- (IBAction)changePerson:(id)sender;
+- (void)loadPeopleFromFiles;
+- (void)loadMessagesFromFiles;
 
 @end
--- PMMessage.m
+++ PMMessage.m
@@ -11,14 +11,40 @@
 
 @implementation PMMessage
 
+- (id)init {
+	[super init];
+	indexOfCurrentPerson = -1;
+	[self loadPeopleFromFiles];
+	NSLog(@"%@", peopleArray);
+	return self;
+}
+
+- (void)enableInterface {
+	[message setEnabled:YES];
+	[response setEnabled:YES];
+	[save setEnabled:YES];
+	[copy setEnabled:YES];
+}
+
+- (void)disableInterface {
+	[message setEnabled:NO];
+	[response setEnabled:NO];
+	[save setEnabled:NO];
+	[copy setEnabled:NO];
+}
+
 - (void)awakeFromNib {
 	NSLog(@"Awoken!");
 	[status setStringValue:PMInitializedText];
-	//NSFont *font = [NSFont fontWithName:@"Kozuka Mincho Pro" size:16.0];
-	NSFont *font = [NSFont fontWithName:@"Helvetica" size:16.0];
-	response.font = font;
-	message.font = font;
-	[self loadFromFiles];
+	[self setFont:PMHelveticaFont];
+	[people addItemsWithTitles:peopleArray];
+	if (indexOfCurrentPerson >= 0) {
+		[self loadMessagesFromFiles];
+		[self enableInterface];
+	}else{
+		[status setStringValue:PMConcatenateToString([status stringValue], PMSelectAPersonText)];
+		[self disableInterface];
+	}
 }
 
 - (IBAction)save:(id)sender {
@@ -26,9 +52,10 @@
 	// Get text and put in buffer.
 	messageText = [message stringValue];
 	responseText = [response stringValue];
+	NSLog(@"%@, %@", messageFile, responseFile);
 	// Write to text files.
 	NSError *error;
-	if(![messageText writeToFile:messageFile atomically:YES encoding:NSUTF8StringEncoding error:&error] || ![responseText writeToFile:responseFile atomically:YES encoding:NSUTF8StringEncoding error:&error]){
+	if(![messageText writeToFile:messageFile atomically:YES encoding:NSUTF8StringEncoding error:&error] || ![responseText writeToFile:responseFile atomically:YES encoding:NSUTF8StringEncoding error:&error] || ![self savePeopleToFile]){
 		NSLog(@"We have a problem: %@", [error localizedFailureReason]);
 		[status setStringValue:PMErrorSavingFiles([error localizedFailureReason])];
 	}else{
@@ -37,7 +64,12 @@
 	}
 }
 
+- (BOOL)savePeopleToFile {
+	return [peopleArray writeToFile:peopleFile atomically:YES];
+}
+
 - (IBAction)copyResponseToClipboard:(id)sender {
+	[self save:nil];
 	// Take everything from response and put it in the clipboard.
 	NSPasteboard *pboard = [NSPasteboard generalPasteboard];
 	[pboard clearContents];
@@ -48,14 +80,138 @@
 	}
 }
 
-- (void)loadFromFiles {
+- (IBAction)chooseFont:(id)sender{
+	
+}
+
+- (void)setFont:(NSFont *)aFont{
+	response.font = aFont;
+	message.font = aFont;
+}
+
+- (IBAction)changePerson:(id)sender{
+	NSString *tempPerson = [[sender selectedItem] title];
+	if ([tempPerson compare:@"New..."] == NSOrderedSame) {
+		[self save:nil];
+		NSLog(@"New person to be created! Hurrah!");
+		NSTextField *accessory = [[NSTextField alloc] initWithFrame:NSMakeRect(0,0,200,20)];
+		NSFont *font = [NSFont systemFontOfSize:[NSFont systemFontSize]];
+		NSDictionary *textAttributes = [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
+		[accessory insertText:[[NSAttributedString alloc] initWithString:@"Who was it for?" attributes:textAttributes]];
+		[accessory setEditable:YES];
+		[accessory setDrawsBackground:NO];
+		
+		NSAlert *chooseAPerson = [[NSAlert alloc] init];
+		[chooseAPerson setAlertStyle:NSInformationalAlertStyle];
+		[chooseAPerson setMessageText:PMNewPersonText];
+		[chooseAPerson setAccessoryView:accessory];
+		[chooseAPerson runModal];
+		// GET THAT RESPONSE!!!
+		NSString *person = [accessory stringValue];
+		[peopleArray addObject:person];
+		indexOfCurrentPerson = [peopleArray indexOfObject:person];
+		[people addItemWithTitle:person];
+		[people selectItemWithTitle:person];
+		NSString *libPath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+		NSString *folderPath = [[libPath stringByAppendingPathComponent:@"Application Support"] stringByAppendingPathComponent:@"Responding"];
+		messageFile = [folderPath stringByAppendingPathComponent:PMMessageTo(PMCurrentPerson)];
+		responseFile = [folderPath stringByAppendingPathComponent:PMResponseTo(PMCurrentPerson)];
+		NSLog(@"Recipient added '%@' at index %d", person, indexOfCurrentPerson);
+		NSLog(@"%@, %@", messageFile, responseFile);
+		[messageFile retain];
+		[responseFile retain];
+		[status setStringValue:PMNewPersonCreated];
+		[message setStringValue:@""];
+		[response setStringValue:@""];
+		[self savePeopleToFile];
+		[self enableInterface];
+	}else{
+		NSLog(@"Changing to '%@'", tempPerson);
+		// Load the messages!
+		indexOfCurrentPerson = [peopleArray indexOfObject:tempPerson];
+		NSLog(@"%d", indexOfCurrentPerson);
+		[status setStringValue:PMInitializedText];
+		[self loadMessagesFromFiles];
+		[self enableInterface];
+	}
+}
+
+- (void)loadPeopleFromFiles {
+	NSString *libPath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+	NSString *folderPath = [[libPath stringByAppendingPathComponent:@"Application Support"] stringByAppendingPathComponent:@"Responding"];
+	peopleFile = [folderPath stringByAppendingPathComponent:@"people.plist"];
+	[peopleFile retain];
+	BOOL yah = YES;
+	if([[NSFileManager defaultManager] fileExistsAtPath:folderPath isDirectory:&yah]){
+		// Is the file there?
+		if ([[NSFileManager defaultManager] fileExistsAtPath:peopleFile]) {
+			// They sure do! Great. Load 'em!
+			peopleArray = [[NSMutableArray alloc] initWithContentsOfFile:peopleFile];
+		}else{
+			// Guess it isn't. Gah!
+			peopleArray = [[NSMutableArray alloc] init];
+			// Are there left-over message and response files from before?
+			messageFile = [folderPath stringByAppendingPathComponent:@"message.txt"];
+			responseFile = [folderPath stringByAppendingPathComponent:@"response.txt"];
+			if ([[NSFileManager defaultManager] fileExistsAtPath:messageFile] && [[NSFileManager defaultManager] fileExistsAtPath:responseFile]) {
+				// They're there! Recover them. But first: ask for a name!
+				NSTextField *accessory = [[NSTextField alloc] initWithFrame:NSMakeRect(0,0,200,20)];
+				NSFont *font = [NSFont systemFontOfSize:[NSFont systemFontSize]];
+				NSDictionary *textAttributes = [NSDictionary dictionaryWithObject:font forKey:NSFontAttributeName];
+				[accessory insertText:[[NSAttributedString alloc] initWithString:@"Who was it for?" attributes:textAttributes]];
+				[accessory setEditable:YES];
+				[accessory setDrawsBackground:NO];
+				
+				NSAlert *chooseAPerson = [[NSAlert alloc] init];
+				[chooseAPerson setAlertStyle:NSInformationalAlertStyle];
+				[chooseAPerson setMessageText:PMNoPeopleFoundText];
+				[chooseAPerson setInformativeText:PMResponseFoundButNoPeopleText];
+				[chooseAPerson setAccessoryView:accessory];
+				[chooseAPerson runModal];
+				// GET THAT RESPONSE!!!
+				NSString *person = [accessory stringValue];
+				[peopleArray addObject:person];
+				indexOfCurrentPerson = 0;
+				NSLog(@"Person was: %@", person);
+				// Move files!
+				NSString *newPath = [[messageFile stringByDeletingLastPathComponent] stringByAppendingPathComponent:PMMessageTo(PMCurrentPerson)];
+				[[NSFileManager defaultManager] moveItemAtPath:messageFile toPath:newPath error:nil];
+				newPath = [[responseFile stringByDeletingLastPathComponent] stringByAppendingPathComponent:PMResponseTo(PMCurrentPerson)];
+				[[NSFileManager defaultManager] moveItemAtPath:responseFile toPath:newPath error:nil];
+				[chooseAPerson release];
+			}else {
+				// Bummer. Must be a fresh install or something.
+				NSAlert *chooseAPerson = [[NSAlert alloc] init];
+				[chooseAPerson setAlertStyle:NSInformationalAlertStyle];
+				[chooseAPerson setMessageText:PMNoPeopleFoundText];
+				[chooseAPerson setInformativeText:PMNoPeopleFoundText];
+				[chooseAPerson runModal];
+				[chooseAPerson release];
+			}
+		}
+	}else{
+		// Nothing exists.
+		if(![[NSFileManager defaultManager] createDirectoryAtPath:folderPath withIntermediateDirectories:YES attributes:nil error:NULL]){
+			NSLog(@"Error: Create folder failed %@", folderPath);
+			peopleArray = [[NSMutableArray alloc] init];
+			[status setStringValue:@"Could not create folder. Please quit and relaunch the program."];
+		}else {
+			// Create files.
+			peopleArray = [[NSMutableArray alloc] init];
+			[status setStringValue:PMConcatenateToString([status stringValue], PMInitializedWithNewText)];
+		}
+	}
+	
+}
+
+- (void)loadMessagesFromFiles {
 	// Load text from files. We will use 2 separate files, one for the response, one for the message.
 	
 	// Do they exist?
 	NSString *libPath = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
 	NSString *folderPath = [[libPath stringByAppendingPathComponent:@"Application Support"] stringByAppendingPathComponent:@"Responding"];
-	messageFile = [folderPath stringByAppendingPathComponent:@"message.txt"];
-	responseFile = [folderPath stringByAppendingPathComponent:@"response.txt"];
+	messageFile = [folderPath stringByAppendingPathComponent:PMMessageTo(PMCurrentPerson)];
+	responseFile = [folderPath stringByAppendingPathComponent:PMResponseTo(PMCurrentPerson)];
 	BOOL yah = YES;
 	NSLog(@"%@\n%@\n%@", folderPath, messageFile, responseFile);
 	if ([[NSFileManager defaultManager] fileExistsAtPath:folderPath isDirectory:&yah]) {
@@ -63,6 +219,7 @@
 		if ([[NSFileManager defaultManager] fileExistsAtPath:messageFile] && [[NSFileManager defaultManager] fileExistsAtPath:responseFile]) {
 			// Load.
 			NSLog(@"They exist!");
+			[people selectItemWithTitle:[peopleArray objectAtIndex:indexOfCurrentPerson]];
 			messageText = [[NSString alloc] initWithContentsOfFile:messageFile];
 			responseText = [[NSString alloc] initWithContentsOfFile:responseFile];
 			[message setStringValue:messageText];
@@ -90,6 +247,7 @@
 	}
 	[messageFile retain];
 	[responseFile retain];
+	[self savePeopleToFile];
 }
 
 - (void)dealloc {
