3295f141cd6e1f17af0ab3a84468a671cb811e06
Parker Moore <parkrmoore@gmail.com>
2013-08-11 01:33:39 +0200

    Create Responding.app's AppSupport directory if not there
    In order to save the txt files that contain the messages and
    responses, it is important to create the directory in which
    these files will go. In this case, we create:
      ~/Library/Application Support/Responding
    
    It is in this directory that we store "message_from*.txt" and
    "message_to*.txt" and must exist for state-saving to work.

--- RespondingAppDelegate.h
+++ RespondingAppDelegate.h
@@ -14,6 +14,7 @@
 
 @property (assign) IBOutlet NSWindow *window;
 
+- (BOOL)findOrCreateApplicationSupportDirectory;
 - (IBAction)openHelpPage:(id)sender;
 
 @end
--- RespondingAppDelegate.m
+++ RespondingAppDelegate.m
@@ -18,10 +18,53 @@
 	minSize.width = 900;
 	minSize.height = 563;
 	[window setMinSize:minSize];
+
+    // ensure the Application Support directory exists
+    [self findOrCreateApplicationSupportDirectory];
+}
+
+- (BOOL)findOrCreateApplicationSupportDirectory {
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(
+        NSApplicationSupportDirectory,
+        NSUserDomainMask,
+        YES);
+    if ([paths count] == 0) {
+        NSLog(@"Your application support directory has not been found. Weird.");
+        return NO;
+    }
+
+    NSString *appSupportDir = [paths objectAtIndex:0];
+    appSupportDir = [appSupportDir stringByAppendingPathComponent:@"Responding"];
+    NSLog(@"appSupportDir = %@", appSupportDir);
+
+    BOOL exists;
+    BOOL isDirectory;
+    exists = [[NSFileManager defaultManager]
+              fileExistsAtPath:appSupportDir
+              isDirectory:&isDirectory];
+
+    if (exists) {
+        NSLog(@"Your application support dir already exists: %@", appSupportDir);
+        return YES;
+    }
+    if (!isDirectory) {
+        NSLog(@"The application support dir exists but is not a directory... what?");
+    }
+    NSError *error;
+    BOOL success = [[NSFileManager defaultManager]
+                    createDirectoryAtPath:appSupportDir
+                    withIntermediateDirectories:NO
+                    attributes:nil
+                    error:&error];
+    if (!success) {
+        return NO;
+    }
+    NSLog(@"%@ has been created.", appSupportDir);
+    return YES;
 }
 
 - (IBAction)openHelpPage:(id)sender{
-	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://www.parkermoore.de/projects/RespondingApp/"]];
+	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://blog.parkermoore.de/Responding/"]];
 }
 
 @end
